f96a86b2894a7680469710e89797ccff74030591
Mucenicalexandru <mucenic.alexandru@gmail.com>
2021-01-11 14:43:59 +0200

    Dependencies added

---
+++ src/main/java/com/switchplaybackend/demo/SwitchPlayApplication.java
@@ -0,0 +1,13 @@
+package com.switchplaybackend.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwitchPlayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SwitchPlayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/switchplaybackend/demo/SwitchPlayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.switchplaybackend.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwitchPlayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
