08b99a91892a2e8ec4019aae236b34930b028388
soare23 <toma.mihai.sorin23@gmail.com>
2021-01-12 12:41:36 +0200

    builtd offer DB repository

---
+++ src/main/java/com/switchplaybackend/demo/repository/OfferRepository.java
@@ -0,0 +1,10 @@
+package com.switchplaybackend.demo.repository;
+
+import com.switchplaybackend.demo.model.Offer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface OfferRepository extends JpaRepository<Offer, UUID> {
+
+}
