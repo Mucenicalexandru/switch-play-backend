3ca6a94290421446f4c5c9ed849eb717e435cabd
Mucenicalexandru <mucenic.alexandru@gmail.com>
2021-01-12 10:59:55 +0200

    Game Controller

--- src/main/java/com/switchplaybackend/demo/model/Game.java
+++ src/main/java/com/switchplaybackend/demo/model/Game.java
@@ -1,10 +1,12 @@
 package com.switchplaybackend.demo.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 import lombok.Data;
 import org.hibernate.annotations.GenericGenerator;
 
 import javax.persistence.*;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
@@ -28,8 +30,8 @@ public class Game {
     private String platform;
     private String picture;
 
-    @OneToMany(mappedBy = "game")
-    @JsonManagedReference(value = "game")
-    private List<Category> category;
+    @OneToMany(mappedBy = "game", cascade = CascadeType.ALL, orphanRemoval = true)
+    @JsonManagedReference()
+    private List<Category> category = new ArrayList<>();
 
 }
