fe23af964eacdc0c26b9aa5a47b76edc15873088
soare23 <toma.mihai.sorin23@gmail.com>
2021-01-14 12:29:32 +0200

    added route to get user by id

--- src/main/java/com/switchplaybackend/demo/api/UserController.java
+++ src/main/java/com/switchplaybackend/demo/api/UserController.java
@@ -3,18 +3,17 @@ package com.switchplaybackend.demo.api;
 import com.switchplaybackend.demo.model.User;
 import com.switchplaybackend.demo.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.web.servlet.server.Session;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.security.crypto.bcrypt.BCrypt;
-
-import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/api")
@@ -26,6 +25,12 @@ public class UserController {
 
     @GetMapping("/users")
     public List<User> getAllUsers(){ return userRepository.findAll();}
+
+
+    @GetMapping("/get-user-by-id/{id}")
+    public Optional<User> getUserByID(@PathVariable UUID id){
+        return userRepository.findById(id);
+    }
 
 
     @PostMapping("/add-user")
@@ -48,12 +53,15 @@ public class UserController {
         if(userRepository.existsByEmail(user.getEmail())){
             if(BCrypt.checkpw(user.getPassword(), userRepository.findByEmail(user.getEmail()).getPassword())){
                 HttpHeaders responseHeaders = new HttpHeaders();
+                System.out.println("correct password");
                 return new ResponseEntity<>(responseHeaders, HttpStatus.OK);
             }else{
+                System.out.println("wrong password");
                 HttpHeaders responseHeaders = new HttpHeaders();
                 return new ResponseEntity<>(responseHeaders, HttpStatus.UNAUTHORIZED);
             }
         }else{
+            System.out.println("wrong email");
             HttpHeaders responseHeaders = new HttpHeaders();
             return new ResponseEntity<>(responseHeaders, HttpStatus.CONFLICT);
         }
