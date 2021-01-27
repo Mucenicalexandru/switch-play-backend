4124805117486e26fa7de1d05f5eb18f4280b5a9
mihaicaragea <61205164+mihaicaragea@users.noreply.github.com>
2021-01-27 11:29:56 +0200

    get access for login and register

--- src/main/java/com/switchplaybackend/demo/security/SecurityConfig.java
+++ src/main/java/com/switchplaybackend/demo/security/SecurityConfig.java
@@ -34,6 +34,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                     .addFilterBefore(new JwtTokenFilter(jwtTokenServices), UsernamePasswordAuthenticationFilter.class)
                     .authorizeRequests()
                     .antMatchers("/api/get-offer/**").permitAll()
+                    .antMatchers("/api/login").permitAll()
+                    .antMatchers("/api/add-user/").permitAll()
                 .anyRequest().authenticated();
 
 
